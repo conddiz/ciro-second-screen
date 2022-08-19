7b4ed51e809b5710ab76f8ce5606f4ccf479b32d
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2022-08-19 13:03:25 -0300

    Cabeçalho

--- src/components/templates/Header.js
+++ src/components/templates/Header.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
-import { Box } from '@mui/material'
+import Image from 'next/image'
+
+import { Box, Container } from '@mui/material'
 
 const Header = ({ children }) => {
-    return <Box component="header">{children}</Box>
+    return (
+        <Box
+            component="header"
+            sx={{
+                display: 'flex',
+                bgcolor: 'tertiary.darker',
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                px: '30px',
+                py: '15px',
+            }}
+        >
+            <Box></Box>
+            <Image
+                src="/img/logo-prefiro-ciro-ana-paula.png"
+                alt="Logo"
+                width="180px"
+                height="58px"
+            />
+            <Box></Box>
+        </Box>
+    )
 }
 
 export { Header }

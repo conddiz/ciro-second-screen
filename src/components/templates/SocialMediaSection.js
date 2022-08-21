8b70ca000f0a4c49ee5f8d5af980000ee8ca7fc7
unknown <roberto.coelho@gmail.com>
2022-08-21 18:44:34 -0300

    Social media links and icons

--- src/components/templates/SocialMediaSection.js
+++ src/components/templates/SocialMediaSection.js
@@ -2,6 +2,10 @@ import Image from 'next/image'
 
 import { Box } from '@mui/material'
 
+import { SOCIAL } from '@ciro/constants'
+
+import Link from 'next/link'
+
 const SocialMediaSection = ({ children }) => {
     return (
         <Box
@@ -11,16 +15,104 @@ const SocialMediaSection = ({ children }) => {
                 bgcolor: 'tertiary.darker',
                 flexDirection: 'row',
                 justifyContent: 'space-between',
-                alignItems: 'center',
+                alignItems: 'flex-end',
                 px: '30px',
-                py: '15px',
-                height: '200px',
+                py: '0',
+                height: '304px',
+                h2: {
+                    display: 'inline-block',
+                    fontSize: '83px',
+                    background: 'linear-gradient(to right, #FF9D00, #FF9D00, #49BA27, #49BA27, #0096ED, #0096ED, #0096ED)',
+                    WebkitBackgroundClip: 'text',
+                    WebkitTextFillColor: 'transparent',
+                    fontFamily: 'New Grotesk Square',
+                    maxWidth: '737px',
+                    fontWeight: '200',
+
+                    '> span:nth-child(1)': {
+                        fontWeight: '300',
+                    },
+                    '> span:nth-child(2)': {
+                        fontWeight: '500',
+                    },
+                    '> span:nth-child(3)': {
+                        fontWeight: '900',
+                    }
+                },
+                ul: {
+                    margin: '-70px 0 0 -34px',
+                    display: 'flex',
+                    alignItems: 'flex-start',
+                    justifyContent: 'center',
+                    columnGap: '20px',
+                    listStyle: 'none',                    
+                },
+                a: {
+                    width: '42px',
+                    height: 'auto',
+                    display: 'block',
+                    position: 'relative',
+                    "&:hover": {
+                        "&:after": {
+                            opacity: 1,
+                        },
+                    },
+                    "&:after": {
+                        content: '""',
+                        width: '64px',
+                        height: '64px',
+                        backgroundColor: 'rgba(255, 255, 255, .1)',
+                        position: 'absolute',
+                        top: '50%',
+                        left: '51%',
+                        transform: 'translate(-50%, -50%)',
+                        borderRadius: '50%',
+                        opacity: 0,
+                        transition: 'all .2s ease -in',
+                    }
+                }                
+                
             }}
         >
-            <Box></Box>
+            <Box
+            sx={{
+                flex: 1,
+                display: 'flex',
+                flexDirection: 'column',
+                alignContent: 'center',
+                textAlign: 'center',
+                // rowGap: '30px',
+                height: '100%',
+            }}>
+                <h2>VEM <span>PRA</span> <span>TURMA</span> <span>BOA</span></h2>
+                <ul>                    
+                    {
+                    SOCIAL.map(({ id, url, text, src, target, rel, ...props }) => (
+                        <li key={id}>
+                        <Link href={url+text}>
+                            <a {...props}>
+                            <Image src={src} alt="Ícone" layout="responsive" width="24" height="24" />
+                            </a>
+                        </Link>
+                        </li>
+                    ))
+                    }
+                </ul>
+            </Box>
 
-            <Box></Box>
-        </Box>
+            <Box 
+            sx={{            
+                display: 'flex',
+                alignItems: 'flex-end',                                                   
+            }}>
+                <Image 
+                    src="/img/ciro12.png" 
+                    alt="Ciro Gomes 12" 
+                    width={"358"} 
+                    height={"342"}                     
+                />
+            </Box>
+        </Box >
     )
 }
 
---
+++ src/constants/socials.js
@@ -0,0 +1,66 @@
+export const SOCIAL = [
+    {
+        id:'youtube',
+        url: 'https://www.youtube.com/c/CiroGomesOficial?sub_confirmation=1',
+        src: '/icons/youtube.svg',
+        target: '_blank',
+        rel: 'noopener noreferrer',
+    },
+    {
+        id:'instagram',
+        url: 'https://www.instagram.com/cirogomesoficial/',
+        src: '/icons/instagram.svg',
+        target: '_blank',
+        rel: 'noopener noreferrer',
+    },
+    {
+        id:'facebook',
+        url: 'https://www.facebook.com/cirogomesoficial/',
+        src: '/icons/facebook.svg',
+        target: '_blank',
+        rel: 'noopener noreferrer',
+    },
+    {
+        id:'twitter',
+        url: 'https://twitter.com/cirogomesoficial',
+        src: '/icons/twitter.svg',
+        target: '_blank',
+        rel: 'noopener noreferrer',
+    },
+    {
+        id:'tiktok',
+        url: 'https://www.tiktok.com/@cirogomesoficial',
+        src: '/icons/tiktok.svg',
+        target: '_blank',
+        rel: 'noopener noreferrer',
+    },
+    {
+        id:'whatsapp',
+        url: 'https://api.whatsapp.com/send?text=',
+        text: 'https://cirogomes.com.br/#rebeldia',
+        src: '/icons/whatsapp.svg',
+        target: '_blank',
+        rel: 'noopener noreferrer',
+    },
+    {
+        id:'linkedin',
+        url: 'https://www.linkedin.com/company/ciro-gomes-oficial/',
+        src: '/icons/linkedin.svg',
+        target: '_blank',
+        rel: 'noopener noreferrer',
+    },
+    {
+        id:'telegram',
+        url: 'https://t.me/cirogomesoficial',
+        src: '/icons/telegram.svg',
+        target: '_blank',
+        rel: 'noopener noreferrer',
+    },
+    {
+        id:'waze',
+        url: 'https://waze.com/ul?acvp=83E482C2-5698-4053-8DC3-87F874FCC6BA',
+        src: '/icons/waze.svg',
+        target: '_blank',
+        rel: 'noopener noreferrer',
+    }    
+]
